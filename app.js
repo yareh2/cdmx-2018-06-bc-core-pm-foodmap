4e850917bfe73ed56f16eae6cf023326f46d7367
yareh2 <hatena5_gir@hotmail.com>
2018-08-06 18:59:52 -0500

    funciona el splash

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+
+  setTimeout(function() {
+    $("#splash").fadeOut(500);
+      }, 3000);
+  setTimeout(function() {
+    $("#main").fadeIn(500);
+  }, 3000);
+
+});
